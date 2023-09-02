635dee261037b60b30556abe4ea9b695c6094afa
anselmo <anselmoahmed@gmail.com>
2023-09-02 03:45:13 -0300

    next_storybook_jest_react

---
+++ src/templates/Base/test.jsx
@@ -0,0 +1,19 @@
+// import { renderTheme } from '../../styles/render-theme';
+// import { Base } from '.';
+// import { mockBase } from './mock';
+// import { fireEvent, screen } from '@testing-library/react';
+
+// describe('<Base />', () => {
+//   it('should render', () => {
+//     const { container } = renderTheme(<Base {...mockBase} />);
+//     expect(container).toMatchSnapshot();
+//   });
+
+//   it('should scrollTo top', () => {
+//     e['preventDefault'] = vi.fn();
+//     renderTheme(<Base {...mockBase} />);
+//     const goTop = screen.getByLabelText(/Go to top/i);
+//     fireEvent.click(goTop);
+//     expect(e.preventDefault).toHaveBeenCalled();
+//   });
+// });
