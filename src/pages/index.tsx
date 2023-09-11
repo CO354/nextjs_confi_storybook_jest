11638b8740db534c34d013582e69ca0cfa71b37c
CO354 <anselmoahmed@gmail.com>
2023-09-11 19:04:47 +0200

    updare

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Home from '../templates/Home';
 import { loadPages } from '../api/load-pages';
 import { GetStaticProps } from 'next';
-const a = 4;
+const a = 6;
 export type IndexProps = {
   data: [];
 };
